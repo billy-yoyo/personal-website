67ba690c567e73ed0d60eca5b4df8d61abada130
Will Page <Will.Page@softwire.com>
2024-04-30 13:37:44 +0100

    rename module in start.sh

--- start.sh
+++ start.sh
@@ -3,8 +3,8 @@ cp -r ./infrastructure/. /etc/nginx/
 sudo systemctl reload nginx
 
 source .venv/bin/activate
-python3 manage.py migrate --settings league_fantasy.settings.prod
-python3 manage.py collectstatic --settings league_fantasy.settings.prod
+python3 manage.py migrate --settings personal_website.settings.prod
+python3 manage.py collectstatic --settings personal_website.settings.prod
 
 pkill -f gunicorn
-nohup gunicorn league_fantasy.wsgi --env DJANGO_SETTINGS_MODULE=league_fantasy.settings.prod &
+nohup gunicorn personal_website.wsgi --env DJANGO_SETTINGS_MODULE=personal_website.settings.prod &
