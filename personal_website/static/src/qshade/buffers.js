774052223157d3efbd2ae6c68e3a16afbb2d96f0
Will Page <Will.Page@softwire.com>
2024-04-30 15:07:08 +0100

    delay image load cllback

--- personal_website/static/src/qshade/buffers.js
+++ personal_website/static/src/qshade/buffers.js
@@ -97,23 +97,25 @@ const ShaderBuffers = (function() {
 
         const image = new Image();
         image.onload = () => {
-            texObj.width = image.width;
-            texObj.height = image.height;
+            setTimeout(() => {
+                texObj.width = image.width;
+                texObj.height = image.height;
 
-            gl.bindTexture(gl.TEXTURE_2D, texture);
-            gl.texImage2D(gl.TEXTURE_2D, 0, gl[format], gl[format], gl[type], image);
+                gl.bindTexture(gl.TEXTURE_2D, texture);
+                gl.texImage2D(gl.TEXTURE_2D, 0, gl[format], gl[format], gl[type], image);
 
-            if (isPowerOf2(image.width) && isPowerOf2(image.height)) {
-                gl.generateMipmap(gl.TEXTURE_2D);
-            } else {
-                gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
-                gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
-                gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
-            }
+                if (isPowerOf2(image.width) && isPowerOf2(image.height)) {
+                    gl.generateMipmap(gl.TEXTURE_2D);
+                } else {
+                    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+                    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+                    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+                }
 
-            if (opts.callback) {
-                opts.callback(texObj, image);
-            }
+                if (opts.callback) {
+                    opts.callback(texObj, image);
+                }
+            });
         };  
 
         image.src = opts.src;
